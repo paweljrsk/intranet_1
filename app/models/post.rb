81e1a5959b304116c1644c6a3f5ede4ecafeb81c
Mikołaj Zygadło <mzygadlo@zendesk.com>
2020-12-25 11:59:39 +0100

    Fix model inheritance If you use method 'belong_to' use a singular

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :product_types
+  belongs_to :product_type
 
   validates :title, presence: true
   validates :description, presence: true
   validates :long_description, presence: true
   validates :product_type_id, presence: true
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-product_categories = (1..10).map do |i|
+product_categories = (1..5).map do |i|
   ProductCategory.create!(name: "Category_#{i}")
 end
 
 types = (1..10).map do |i|
-  ProductType.create!(product_category: product_categories[i - 1],
+  ProductType.create!(product_category: product_categories[i % 5 - 1],
                       name: "Type_#{i}")
 end
 
